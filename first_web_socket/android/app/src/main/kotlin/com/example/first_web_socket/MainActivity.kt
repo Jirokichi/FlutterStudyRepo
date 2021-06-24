66a0f07af7ffcba89e1b529661e195b696ae0bba
Chorokichi <kdy.developer@gmail.com>
2021-06-24 16:26:41 +0900

    first_web_socket用にflutterのNew Projectを追加

---
+++ first_web_socket/android/app/src/main/kotlin/com/example/first_web_socket/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.first_web_socket
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ sample_file_app/android/app/src/main/kotlin/com/example/sample_file_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sample_file_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
